608cb062c7f90b488b4ca7cf9a77ff8c11fdd707
kimmin <kimmin2301@gmail.com>
2020-12-15 23:25:39 +0900

    fetch-join init

---
+++ fetch-join/src/main/java/com/mins/fetchjoin/FetchJoinApplication.java
@@ -0,0 +1,13 @@
+package com.mins.fetchjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchJoinApplication.class, args);
+	}
+
+}
---
+++ fetch-join/src/test/java/com/mins/fetchjoin/FetchJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mins.fetchjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
